600747b4b7576e8531025adb49c036b3d67b2870
Shilpa pal <shilpapal1721@gmail.com>
2025-06-24 16:56:00 +0530

    if and else problem sloved.

--- python-code/if_esle.py
+++ python-code/if_esle.py
@@ -17,3 +17,20 @@ for Javascript : console.log()
 
 """
 
+def studentMarks(marks):
+
+    if marks >=90:
+        print("grade is A")
+    elif marks >=70:
+        print(" garde is B")
+    elif marks >=50:
+        print("grade is c")
+    elif marks >=35:
+        print("grade is D")
+    else:
+        print("grade is E")
+        
+
+
+marks = 95
+studentMarks(marks)   
