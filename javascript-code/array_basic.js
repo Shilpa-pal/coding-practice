318122535fe1896bd9226df8346cde686eb1c1ef
Shilpa pal <shilpapal1721@gmail.com>
2025-09-18 18:56:37 +0530

    practice more problems and practice brute force solution also .

--- javascript-code/array_basic.js
+++ javascript-code/array_basic.js
@@ -111,3 +111,113 @@ function removeDuplicates(rray){
     return i + 1
 }
 console.log(removeDuplicates([3, 3, 5, 6, 6, 7]))
+
+//aaray move element to the left rotate by on palce
+//n length = 6
+function moveElmnetLeft(arr){
+    n = arr.length
+    let temp = arr[0]  // because the 0th index we want to move right
+    for(let i = 1;i<n;i++){
+        arr[i-1] = arr[i] // it help to shift the number arr[i-1]=0index and arr[i]means arr[1st]inx it will keep rais ebt each iteration 
+    }
+    arr[n-1] = temp;
+    return arr
+
+}
+console.log(moveElmnetLeft([1,3,4,5,6,7]))
+
+//brute force approach solution aaray move element to the left rotate by on palce
+function rotateLeftBrute(arr) {
+    let n = arr.length;
+    let temp = [];
+
+    // Step 1: copy from index 1 to n-1
+    for (let i = 1; i < n; i++) {
+        temp.push(arr[i]);
+    }
+    // Step 2: push first element at the end
+    temp.push(arr[0]);
+    return temp;
+}
+
+console.log(rotateLeftBrute([1, 2, 3, 4, 5])); // [2,3,4,5,1]
+
+// left rotate by d palace  dmeans double d place d  = 3
+// mean we have to roate last 3 element to start it means last 3 elements come at first then after other number comes 
+//...............................................Brute force approch..........................
+
+function leftRotate(arr,d){
+    let n = arr.length
+    d = d % n // handle if d greater than n
+    let temp = []
+    for(let i = 0;i<d;i++){
+        temp[i]=arr[i]
+    
+    }
+    for(let i = d;i<n;i++){
+        arr[i-d] =arr[i]
+    }
+    for(let i = 0;i<d;i++){
+        arr[n-d+i] = temp[i] // here is formula n(lenth of arr)-d(the number we have to roate )+ i(it start with 0,than one )
+        //(0 goes n-d index,1 goes(n-d)+172 goes(n-d)+2)ind
+    }
+    return arr
+}
+console.log(leftRotate([1, 2, 3, 4, 5, 6, 7], 3));
+// Output: [4, 5, 6, 7, 1, 2, 3]
+
+
+
+
+// Move zero to end 
+
+function moveZero(array){
+    let n = array.length
+   //-1 when you’re dealing with indices/positions to mean “not found.”
+    let j = -1 // assuming we not found any 0number then the below condition would run
+    // it says "So far, I have not yet found any zero"
+    // we taken -1 not any -2,-3 number becuse we took j + 1 if we take j =-2  than j+1 strat with -1 index which is invalid 
+    // so beacuse of it we written -1 
+    for(let i = 0;i<n;i++){
+        if(array[i] == 0){ // if number is == 0
+            j = i;// the moment i found 0 number in array than j become - 1 to j = i where i found zero number & lop get breck and from this idx it start pusing number like shift
+            break// stop looking further | If 0 is at index 4, then j = 4. 
+            // If 0 is at index 1, then j = 1.
+        }
+    }
+    //no Non zero numbers
+    if (j == -1) return array
+
+    for(let i =j+1;i<n;i++){
+        if(array[i] !== 0){ // if number is non 0
+            [array[i],array[j]] = [array[j],array[i]]; //swaping
+            j++
+        }
+    }
+    return array
+}
+console.log(moveZero([1,0,2,3,5,6,0,0,7]))
+
+//move number to zero brute force approach
+
+
+function simpleSolution(arr){
+    let n = arr.length
+    let temp = []
+    for(let i = 0;i<n;i++){
+        if(arr[i]!==0){
+            temp.push(arr[i])
+        }
+    }
+    
+    // Step 2: copy zeros
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+}
+
+console.log(moveZeroBruteForce([1,0,2,3,0,4,0]));
