61171a8ff8d70f7dfafdd26487477a22eefafbb0
Shilpa pal <shilpapal1721@gmail.com>
2025-10-13 23:57:34 +0530

    learn about matrix in this have to marks all 0 htat present in matrix marks 0 by Row and column.

--- javascript-code/arry_codes/Longest_consecutive/practice_diff-version/findPovNumonly.js
+++ javascript-code/arry_codes/Longest_consecutive/practice_diff-version/findPovNumonly.js
@@ -21,7 +21,7 @@ function countConsecutivePositives(arr) {
             let cnt = 1
             let currentNum = num
             // Count next consecutive positive numbers
-                    //currentNum = 4
+            //currentNum = 4
             //cnt = 1
             while (st.has(currentNum + 1)) {  // eg st.has(4 + 1) → st.has(5) → ✅ true
                 currentNum = currentNum + 1  // currentNum = 5
@@ -37,7 +37,8 @@ let arr = [1, 2, -3, 4, 5, 6, -1, 2];
 console.log(countConsecutivePositives(arr))
 
 
-// uisng map same approach 
+// using map Plain Object ({})  same approach 
+// by doing this we need to convert string to number line no.54 if we use Map we don't need to convert it 
 function countConsecutivePositives(arr) {
     let map = {};
     for (let num of arr) {
@@ -45,8 +46,11 @@ function countConsecutivePositives(arr) {
     }
 
     let longest = 0;
-
-    for (let num in map) {
+    //map is a plain object {} → not directly iterable with for…of.
+    //We use for…in because map is an object, and we want to go through its keys (the positive numbers).
+    //We then convert keys to numbers with Number(num) so we can do math with them.
+    for (let num in map) { //plain object  Used for…in
+        //In returns strings, you had to do Number(num) to do arithmetic.
         num = Number(num);
         if (!map[num - 1]) { // start of sequence
             let cnt = 1;
@@ -68,27 +72,60 @@ let array = [1, 2, -3, 4, 5, 6, -1, 2];
 console.log(countConsecutivePositives(array)); // 3
 
 
+//using above by Map
+
+function ConsecutivevOnlybyMap(ar) {
+    let map = new map()
+
+    for (let num of ar) {
+        if (num > 0) map.set(num, true)
+    }
+    let longest = 0
+    //  // no need for Number(num) — it's already a number
+    // Map version Used for…of
+    for (let [num] of map) { //used [num] it give only the key (ignore value)
+        // no need for Number(num) — it's already a number
+
+        if (!map.has(num - 1)) {// start of a sequence
+            let cnt = 1
+            let currentNum = num
+            // keep counting consecutive numbers
+            while (map.has(currentNum + 1)) {//
+                currentNum++;
+                cnt++;
+            }
+            longest = Math.max(longest, cnt);
+
+        }
+         return longest;
+
+    }
+}
+let ar = [1, 2, -3, 4, 5, 6, -1, 2];
+console.log(cConsecutivevOnlybyMaps(ar)); // 3
+
+
 //finding postive number only using sorting
 
-function countConsecutivePositives(array2) {
-    array2 = array2.filter(x => x > 0); // only positive numbers
-    if (array2.length === 0) return 0;
+    function countConsecutivePositives(array2) {
+        array2 = array2.filter(x => x > 0); // only positive numbers
+        if (array2.length === 0) return 0;
 
-    array2.sort((a, b) => a - b); // sort ascending
-    let longest = 1, cnt = 1;
+        array2.sort((a, b) => a - b); // sort ascending
+        let longest = 1, cnt = 1;
 
-    for (let i = 1; i < array2.length; i++) {
-        if (array2[i] === array2[i - 1]) continue; // skip duplicate
-        if (array2[i] === array2[i - 1] + 1) {
-            cnt++;
-        } else {
-            cnt = 1;
+        for (let i = 1; i < array2.length; i++) {
+            if (array2[i] === array2[i - 1]) continue; // skip duplicate
+            if (array2[i] === array2[i - 1] + 1) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+            longest = Math.max(longest, cnt);
         }
-        longest = Math.max(longest, cnt);
+
+        return longest;
     }
 
-    return longest;
-}
-
-let array2 = [1, 2, -3, 4, 5, 6, -1, 2];
-console.log(countConsecutivePositives(array2)); // 3
+    let array2 = [1, 2, -3, 4, 5, 6, -1, 2];
+    console.log(countConsecutivePositives(array2)); // 3
---
+++ javascript-code/arry_codes/matrix/mark metrix zero/mark zero all/bruteForce.js
@@ -0,0 +1,91 @@
+// Set Matrix Zero
+//Given a matrix if an element in the matrix is 0 then you will have to set its entire column and row to 0 and then return the matrix.
+
+// Examples
+// Examples 1:
+// Input: matrix=[[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+// Explanation: Since matrix[2][2]=0.Therfore the 2nd column and 2nd row wil be set to 0.
+ 
+// Input: matrix=[[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// Output:[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+// Explanation:Since matrix[0][0]=0 and matrix[0][3]=0. Therefore 1st row, 1st column and 4th column will be set to 0
+
+
+//approach 1st: mark -1 the all matrix element which coantians 1  but not 0 Element mark -1
+//apprapch 2nd: mark -1 to 0 which row and colum have 0 element 
+
+//note;why we passing matrix in below functio
+//A 2D array is an array of arrays, which represents data in rows and columns
+// like a table or matrix.
+//I passed the matrix so that the helper functions(markRow and markCol) can modify the same 2D array directly,
+
+//In markRow, I pass i because I already know which row has the zero.
+//and inside that function, I loop through all columns j in that row.
+function markRow(matrix,n,m,i){//
+    // set all non-zero elements as -1 in the row i:
+    for(j=0;j<m;j++){
+        if(matrix[i][j] !==0){
+            (matrix[i][j] = -1)
+        }
+    }
+}
+//In markCol, I pass j because I know which column has the zero.
+//I loop through all rows i in that column.
+function markCol(matrix,n,m,j){
+    // set all non-zero elements as -1 in the col j:
+    for(let i =0;i<n;i++){
+        if(matrix[i][j] !==0){
+             (matrix[i][j] = -1)
+        }
+    }
+}
+// Set -1 for rows and cols that contains 0. Don't mark any 0 as -1:
+function markAllzero(matrix,i,j){
+    for(i=0;i<n;i++){
+        for(j =0;j<m;j++){
+            if(matrix[i][j]===0){
+                markRow(matrix,n,m,i)
+                markCol(matrix,n,m,j)
+            }
+        }
+    }
+    // Finally, mark all -1 as 0:
+    for(let i=0;i<n;i++){
+        for(j=0;j<m;j++){
+             if(matrix[i][j]===-1){
+            (matrix[i][j] = 0)     
+        }
+    }
+}
+return matrix
+}
+  const matrix = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ];
+  const n = matrix.length;
+  const m = matrix[0].length;
+    const ans = markAllzero(matrix, n, m);
+  
+  console.log("The Final matrix is: ");
+  for (let i = 0; i < n; i++) {
+    console.log(ans[i].join(" "));
+  }
+
+//time complexity-
+//Nested loops (n outer, m inner) O(n × m)
+//Two separate loops O(n + m)
+//the total time becomes O(n × m × (n + m)).
+//where N = no. of rows in the matrix and M = no. of columns in the matrix.
+// reason: Firstly, we are traversing the matrix to find the cells with the value 0.
+//  It takes O(N*M). Now, whenever we find any such cell we mark that row and 
+// column with -1. This process takes O(N+M). So, combining this the whole process,
+//  finding and marking, takes O((N*M)*(N + M)).
+// Another O(N*M) is taken to mark all the cells with -1 as 0 finally.
+
+//So the total time becomes:
+//O(number of cells)×O(time per cell)
+// =O(n×m)×O(n+m)
+// = O(n×m×(n+m))
