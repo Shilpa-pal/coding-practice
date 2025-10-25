9ce07580b5d158ff8773b9dc6e922bc822f3160a
Shilpa pal <shilpapal1721@gmail.com>
2025-10-25 17:08:58 +0530

    solved and learn about Floor and Ceil  in sorted array.

---
+++ javascript-code/Binary/lower upper bond/floorCeil.js
@@ -0,0 +1,65 @@
+//Problem Statement: You're given an sorted array arr of n integers and an integer x. Find the floor and ceiling of x in arr[0..n-1].
+// The floor of x is the largest element in the array which is smaller than or equal to x.
+// The ceiling of x is the smallest element in the array greater than or equal to x.
+
+// Example 1:
+// Input Format: n = 6, arr[] ={3, 4, 4, 7, 8, 10}, x= 5
+// Result: 4 7
+// Explanation: The floor of 5 in the array is 4, and the ceiling of 5 in the array is 7.
+
+//optimal approach
+function findFloor(arr, n, x) {
+    let low = 0, high = n - 1
+    let ans = -1 //It gives a default “not found” value.
+    //If at the end ans is still -1, it means there was no valid floor or ceil in the array
+     //sefty-Prevents errors when no suitable element exists.
+
+     // floor below use upperbound condition Yes, I know you have doubt in upper bond we were only 
+     //useing > than sign like lower bond we were use >= sign but in upper bound we only remove th equal 
+    //   > → gives the first greater value.
+    // <= → gives the last smaller or equal value.
+    // <= goes till the last possible value (that’s less than or equal to x).
+     while (low <= high) {
+        let mid = Math.floor((low + high) / 2)
+         // maybe an answer 
+         // aplly upperbnd contion and use sign <= smaller than equal to x
+    
+        if (arr[mid] <= x) {
+            ans = arr[mid]
+             // look for a larger floor on the right
+            low = mid + 1
+        } else {
+            high = mid - 1 // look on the left
+        }
+    }
+    return ans
+}
+function findCeil(arr, n, x) {
+    let low = 0, high = n - 1
+    ans = -1
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2)
+         // maybe an answer
+        if (arr[mid] >= x) {
+            ans = arr[mid]
+// look for smaller ceil on the left
+            high = mid - 1
+        } else {
+            low = mid + 1//// look on the right
+        }
+    }
+    return ans
+}
+function getFloorCeil(arr, n, x) {
+    const f = findFloor(arr, n, x)
+    const c = findCeil(arr, n, x)
+    return [f, c]
+}
+
+let arr = [3, 4, 4, 7, 8, 10]
+let n = arr.length
+let x = 5
+const [f, c] = getFloorCeil(arr, n, x) //returns an array of two values.
+//(f, c) → does not work as a tuple in JS, it just evaluates to c.
+console.log(`This Give ans of Floor , ${f} and ${c}`) 
--- javascript-code/Binary/lower upper bond/upperBound.js
+++ javascript-code/Binary/lower upper bond/upperBound.js
@@ -61,16 +61,16 @@ let position = performingDelInsert(b, target1)
 b.splice(position, 1, 12)
 console.log("This will give value of insert and delete. target value is ", target1, ":-", b)
 
-//inserted optimaal aproach
+// search inserted optimaal aproach(here we find the position to inert value so below we will not use splice )
 
 function searchInsert(ar, target2) {
     let n = ar.length
     let low = 0, high = n - 1
-    ans = n
+    ans = n //Default: if target is larger than all elements, insert at the end
     while (low <= high) {
         let mid = Math.floor((low + high) / 2)
         if (ar[mid] >= target2) {
-            ans  = mid
+            ans  = mid //Found a candidate index; may update to a smaller index by searching left
             high = mid - 1
         } else {
             low = mid + 1
@@ -82,5 +82,31 @@ let ar = [1, 2, 4, 7]
 target2 = 4
 let ind = searchInsert(ar,target2)
 
-// ar.splice(ind,0)
 console.log("The index is,",ind)
+
+// here we actually insert the value (we used splice to insert the value)
+//.insertion, we always use the lower bound condition.
+//The lower bound gives you the first position where the value is greater than or equal to the target.
+//So:If the value already exists → we insert before the first occurrence (keeps sorted order stable).
+//If the value doesn’t exist → you insert at the correct sorted position.
+
+function insertvalue(array2,x){
+    let n = array2.length
+    let low = 0;high= n-1
+    let ans = n
+    while(low<=high){
+        let mid = Math.floor((low + high)/2)
+        if(array2[i]>=mid){
+            high = mid-1
+            ans = mid
+        }else{
+            low = mid +1
+        }
+    }
+    return ans
+}
+let array2 = [3, 4, 4, 7, 8, 10]
+let x = 5
+let place = insertValue(array2,x)
+array2.splice(place,0,x)
+console.log("This method is used by splice method to insert actually value .",x ,"is target value",array2)
