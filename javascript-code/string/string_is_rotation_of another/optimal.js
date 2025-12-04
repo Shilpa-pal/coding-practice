ddc1e2877892243aa836bf4d3a3bd50750bfb5b5
Shilpa pal <shilpapal1721@gmail.com>
2025-12-04 16:18:24 +0530

    added optimal solution of ' Check if one string is rotation of another'.

--- javascript-code/string/string_is_rotation_of another/optimal.js
+++ javascript-code/string/string_is_rotation_of another/optimal.js
@@ -1,1 +1,13 @@
+class Solution {
+rotateString(str,goal){
+// Strings must be of the same length to be rotations of each other
+    if(str.length !== goal.length) return false
+// Concatenate str with itself
+    let doubledStr = str + str // the output will be "rotationrotation"
+    return doubledStr.includes(goal)// Check if goal is a substring of s + s
+    // basically includes is inbuild function that cheack goal part exist in doubledStr
+}
 
+}
+const sol = new Solution();
+console.log(sol.rotateString("rotation", "tionrota")); 
