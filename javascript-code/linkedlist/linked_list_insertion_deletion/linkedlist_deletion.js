889cbe4491496605a8af70253083035240fc7a68
Shilpa-Pal <Shilpapal1721@gmail.com>
2026-01-08 10:55:52 +0530

    Add linked list node deleting logic.

---
+++ javascript-code/linkedlist/linked_list_insertion_deletion/linkedlist_deletion.js
@@ -0,0 +1,47 @@
+
+// Deletion of linked list
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null
+    }
+}
+
+class Solution {
+    //create a function to delete a tail node
+    deleteTail(head) {
+        // if list is empty and has one node
+        if (head === null || head.next === null) {
+            return null
+        }
+        //trasverse two second last
+        let curr = head;
+        while(curr.next.next !== null){
+            curr = curr.next;
+        }
+
+        //delation tail node
+        curr.next = null;
+
+        //return updated head
+        return head
+    }
+}
+
+//drive code 
+let head = new Node(100)
+head.next = new Node(200)
+head.next.next = new Node(300)
+
+let obj = new Solution();
+head = obj.deleteTail(head)
+
+//Print list after deletion
+let temp = head
+let result = ""
+while(temp){
+    result += temp.data + " ";
+    temp = temp.next
+}
+console.log(result)
