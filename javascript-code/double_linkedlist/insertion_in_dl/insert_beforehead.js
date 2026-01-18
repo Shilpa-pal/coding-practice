050b77c44bdb5126cea25c9627be48a527b2720e
Shilpa-Pal <Shilpapal1721@gmail.com>
2026-01-18 06:45:26 +0530

    add insert before the tail in doubly linked list.

--- javascript-code/double_linkedlist/insertion_in_dl/insert_beforehead.js
+++ javascript-code/double_linkedlist/insertion_in_dl/insert_beforehead.js
@@ -20,7 +20,7 @@ function convertArrtoDL(arr) {
 
     return head
 }
-
+// insert value before the head
 function insertBeforeHead(head,val){
     // creating newhead = that val is new head, newhead next will be is head, and newHead 
     // previous should pointing to null accroding to new head
@@ -31,6 +31,31 @@ function insertBeforeHead(head,val){
     //once this done i written to the 
     return newHead
 }
+// insert value before the tail
+function insertBeforetail(head,val){
+    if(head.next === null){
+        return insertBeforeHead(head,val)
+    }
+    let tail = head
+    while(tail.next !== null){
+        tail = tail.next
+    }
+
+    // tail.back is prev 
+    let prev = tail.back
+
+    //creating a new node with value = val ,where will bw the next point to the tail
+    //tail.back point to the prev
+    let newNode = new Node(val,tail,prev)
+
+    //prev next should point to newNode our prev is 5 and when we insertig a new Node before the tail(67) than prev.next point to newNode
+    prev.next = newNode
+    //tail.back should point to newNode our tail.back  is 5 and when we insertig a new Node than tail.back point to newNode
+    tail.back = newNode
+    return head
+}
+
+
 
 function print(head) {
     let curr = head
@@ -43,5 +68,12 @@ function print(head) {
 }
 let arr = [23, 4, 5, 67]
 let head = convertArrtoDL(arr)
+
+// insert value before the head
 head = insertBeforeHead(head,20)
+
+// insert value before the tail
+head = insertBeforetail(head,55)
+
+
 print(head)
